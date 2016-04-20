51c64efa6832287141c0e16a25c7deb305421e4d
jnsu <jinsukim.d@gmail.com>
2016-04-20 04:18:48 +0000

    Upgrade Hatchery to use a proxy

--- DataMiningDaemon/crawler/Hatchery.js
+++ DataMiningDaemon/crawler/Hatchery.js
@@ -2,8 +2,8 @@
 var request = require('request');
 var scrapper = require("./../scrapper/brokenLinkChecker.js");
 
-function Hatchery(privateIpAddressList, companyNameList, shoesList, requestInterval) {
-	this.privateIpAddressList = privateIpAddressList;
+function Hatchery(proxyIpAddressList, companyNameList, shoesList, requestInterval) {
+	this.proxyIpAddressList = proxyIpAddressList;
 	this.companyNameList = companyNameList;
 	this.shoesList = shoesList;
 	this.requestInterval = requestInterval;
@@ -13,20 +13,20 @@ function Hatchery(privateIpAddressList, companyNameList, shoesList, requestInter
 // Start the request daemon.
 Hatchery.prototype.startSendingRequests = function() {
 	console.log('startSendingRequests: Starts');
-	console.log('startSendingRequests: ' + 'Using ' + this.privateIpAddressList.length + ' IPs');
+	console.log('startSendingRequests: ' + 'Using ' + this.proxyIpAddressList.length + ' IPs');
 	console.log('startSendingRequests: ' + 'Sending one request every ' + this.requestInterval / 1000 + ' seconds');
 	var context = this;
 
 	// Send request for every 'requestInterval' seconds
-	var privateIpAddressListIndex = 0;
+	var proxyIpAddressListIndex = 0;
 	var companyNameListIndex = 0;
 	var shoesListIndex = 0;
 	this.requestDaemon = setInterval(function() {
 		// Send request
-		sendRequest(context.privateIpAddressList[privateIpAddressListIndex], context.companyNameList[companyNameListIndex], context.shoesList[shoesListIndex]);
+		sendRequest(context.proxyIpAddressList[proxyIpAddressListIndex], context.companyNameList[companyNameListIndex], context.shoesList[shoesListIndex]);
 
 		// Increase indexes by one
-		privateIpAddressListIndex = (privateIpAddressListIndex + 1) % context.privateIpAddressList.length;
+		proxyIpAddressListIndex = (proxyIpAddressListIndex + 1) % context.proxyIpAddressList.length;
 		companyNameListIndex = (companyNameListIndex + 1) % context.companyNameList.length;
 		if(companyNameListIndex == 0) {		// This means the link for this shoes has been checked for all websites
 			shoesListIndex = (shoesListIndex + 1) % context.shoesList.length;
@@ -34,15 +34,16 @@ Hatchery.prototype.startSendingRequests = function() {
 	}, this.requestInterval);
 };
 
-function sendRequest(privateIpAddress, companyName, shoes) {
-	var logPrefix = 'Private Ip Address: ' + privateIpAddress + ',' + ' Company Name: ' + companyName + ',' + ' Shoes Model: ' + shoes.model + ',' + ' Shoes SKU: ' + shoes.sku;
+function sendRequest(proxyIpAddress, companyName, shoes) {
+	var logPrefix = 'proxy Ip Address: ' + proxyIpAddress + ',' + ' Company Name: ' + companyName + ',' + ' Shoes Model: ' + shoes.model + ',' + ' Shoes SKU: ' + shoes.sku;
 
 	var option = {
 		headers: {
 			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'
 		},
-		localAddress: privateIpAddress,
+
+		proxy: 'http://' + proxyIpAddress,
 		uri: "http://www." + companyName + ".com/product/model:" + shoes.model + "/sku:" + shoes.sku
 	};
 
@@ -55,8 +56,6 @@ function sendRequest(privateIpAddress, companyName, shoes) {
 				scrapper.checkForBrokenLink(companyName, response, body);
 			} else if(response != null && response.statusCode == 403) {
 				console.log('BLOCKED: response.statusCode = ' + response.statusCode);
-				// Change Elastic IP Address
-				// AWS.changeElasticIpForPrivateIp(privateIpAddress)
 			}
 		}
 	});
